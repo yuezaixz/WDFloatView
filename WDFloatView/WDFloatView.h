ff52f2bf65df0885d7506037f4648592ba84662b
yuezaixz <303178394@qq.com>
2016-02-16 16:22:19 +0800

    创建控件的View

---
+++ WDFloatView/WDFloatView.h
@@ -0,0 +1,18 @@
+//
+//  WDFloatView.h
+//  WDFloatView
+//
+//  Created by 吴迪玮 on 16/2/16.
+//  Copyright © 2016年 DNT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WDFloatView : UIView
+
+@property (strong, nonatomic) UIView *mainView;
+@property (strong, nonatomic) UIView *promtView;
+
+@property (strong, nonatomic) UIButton *promtBtn;
+
+@end
---
+++ WDFloatView/WDFloatView.m
@@ -0,0 +1,22 @@
+//
+//  WDFloatView.m
+//  WDFloatView
+//
+//  Created by 吴迪玮 on 16/2/16.
+//  Copyright © 2016年 DNT. All rights reserved.
+//
+
+#import "WDFloatView.h"
+
+@implementation WDFloatView
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
